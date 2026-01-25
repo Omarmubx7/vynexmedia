c1640360f568c1d40cab32b420709bbd082a1e87
Omar Mubaidin <24110213@htu.edu.jo>
2026-01-25 22:32:22 +0300

    nue

--- src/components/PortfolioSection.tsx
+++ src/components/PortfolioSection.tsx
@@ -107,7 +107,25 @@ const clientLogos = [
           </p>
           <a href="#contact" className="btn-primary inline-block">
             تواصل معنا
+          import logo1 from '../assets/1.png';
+          import logo2 from '../assets/2.png';
+          import logo3 from '../assets/3.png';
+          import logo4 from '../assets/4.png';
+          import logo5 from '../assets/5.png';
+          import logo6 from '../assets/6.png';
+          import logo7 from '../assets/7.png';
+          import logo8 from '../assets/8.png';
+          import logo9 from '../assets/9.png';
+          import logo10 from '../assets/10.png';
+          import logo11 from '../assets/11.png';
+          import logo12 from '../assets/12.png';
+          import logo13 from '../assets/13.png';
+          import logo14 from '../assets/14.png';
+          import logo15 from '../assets/15.png';
           </a>
+          const clientLogos = [
+            logo1, logo2, logo3, logo4, logo5, logo6, logo7, logo8, logo9, logo10, logo11, logo12, logo13, logo14, logo15
+          ];
         </div>
       </div>
     </section>
